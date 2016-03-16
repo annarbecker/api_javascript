1b35dc7fae92420d3af28aa94c2e59d71814c0f1
Anna Becker and Daren Schaad <anna.r.becker@gmail.com>
2016-03-16 12:00:05 -0700

    Add scss styling.

--- js/user-interface.js
+++ js/user-interface.js
@@ -4,11 +4,13 @@ var kelvinToFahrenheit = require('./../js/temperature-conversion.js').kelvinToFa
 
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
+  $('#weatherLocation').submit(function(event) {
+    event.preventDefault();
     var city = $('#location').val();
+    $('.showWeather').empty();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').prepend("<br>" + "The temperature in " + city + " is " + Math.ceil(kelvinToCelsius(response.main.temp)) + " Celsius" + "<br>" + "The temperature in " + city + " is " + Math.ceil(kelvinToFahrenheit(response.main.temp)) + " Fahrenheit");
+      $('.showWeather').append("<br>" + "The temperature in " + city + " is " + Math.ceil(kelvinToCelsius(response.main.temp)) + " Celsius" + "<br>" + "The temperature in " + city + " is " + Math.ceil(kelvinToFahrenheit(response.main.temp)) + " Fahrenheit");
       console.log(JSON.stringify(response));
       var myCenter = new google.maps.LatLng({lat: response.coord.lat, lng: response.coord.lon});
       map.setCenter(myCenter);
