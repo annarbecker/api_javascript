e2b3ab820ab61c625b8d7c6d9d82bbbc385b6654
Anna Becker and Daren Schaad <anna.r.becker@gmail.com>
2016-03-16 16:51:19 -0700

    Add minor styling

--- js/forecast-interface.js
+++ js/forecast-interface.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var city = $('#destination').val();
     $.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&cnt=5&appid=' + apiKey).then(function(response) {
       for (i = 0 ; i < 5 ; i++){
-        $('#day' + i).append(moment().add(i, 'days').format('dddd') + '<br>High = ' + Math.round(kelvinToFahrenheit(response.list[i].temp.max)) + '&deg;F<br> Low = '+ Math.round(kelvinToFahrenheit(response.list[i].temp.min)) + '&deg;F<br>' + response.list[i].weather[0].description);
+        $('#day' + i).append(moment().add(i, 'days').format('dddd') + '<br>High = ' + Math.round(kelvinToFahrenheit(response.list[i].temp.max)) + '&deg;F<br> Low = '+ Math.round(kelvinToFahrenheit(response.list[i].temp.min)) + '&deg;F<br>' + response.list[i].weather[0].description + '<br>'+ '<br>');
         console.log(moment().add(i, 'days').format('dddd'));
       }
       getLocation();
